584a0e25a67a5d5c43e0f0976c3ed143838e2d0d
Elin Ahlgren <elin.ahlgren@hotmail.com>
2022-08-22 14:33:59 +0200

    changed max pages to 500

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -6,8 +6,8 @@ const Pagination = ({page, changePage, isPreviousData}) => {
     return (
         <div className="d-flex justify-content-between align-items-center">
             <Button disabled={isPreviousData || page <= 1} onClick={() => changePage(prevValue => prevValue - 1)} variant='dark'>Previous</Button>
-                <span>page: {page}</span>
-            <Button disabled={isPreviousData || page >= 500} onClick={() => changePage(prevValue => prevValue + 1)} variant='dark'>Next</Button> {/* TODO: fix disabled */}
+                <span>page: {page}/500</span>
+            <Button disabled={isPreviousData || page >=500} onClick={() => changePage(prevValue => prevValue + 1)} variant='dark'>Next</Button> {/* TODO: fix disabled, check total_pages - why pages < 500 not working */}
         </div>
     )
 }
