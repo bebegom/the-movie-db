8d907f1509c04a6388a235d6465a25f5aafecf79
Elin Ahlgren <elin.ahlgren@hotmail.com>
2022-08-28 18:10:05 +0200

    gave the paginationbar some margin

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,7 +2,7 @@ import {Button} from 'react-bootstrap'
 
 const Pagination = ({page, changePage, isPreviousData}) => {
     return (
-        <div className="d-flex justify-content-between align-items-center">
+        <div className="d-flex justify-content-between align-items-center my-3 w-25 m-auto">
             <Button disabled={isPreviousData || page <= 1} onClick={() => changePage({
                 page: Number(page) - 1,
             })} variant='dark'>Previous</Button>
